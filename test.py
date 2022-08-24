482f8f61fcabf8f4112a50080d037cdfc8ce3e82
Abhinavk941 <88971606+Abhinavk941@users.noreply.github.com>
2022-08-24 22:01:19 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+Details_Of_Students = {'Name' : ["Abhinav","Kushagra","Aaditya","Mukul"], 'Batch' : [3,3,3,3]}
+final_list = pd.DataFrame(Details_Of_Students)
+print(final_list)
